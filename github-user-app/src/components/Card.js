ccbb64120eec35e959e52c397821cdebc09d6fe0
Alison Law <alisparklaw@gmail.com>
2020-02-11 14:08:01 +0000

    Card Styling

--- github-user-app/src/components/Card.js
+++ github-user-app/src/components/Card.js
@@ -1,44 +1,43 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 
 const Card = props => {
   const { login, id, url, avatar_url } = props.user;
   return (
-    <div>
+    <StyledDiv>
       <h1>{login}</h1>
       <h3>id: {id}</h3>
       <h3>url: {url}</h3>
       <img className="avatar" src={avatar_url} alt="avatar pic" />
-    </div>
+    </StyledDiv>
   );
 };
 export default Card;
 
 
-    // styling
-    // const StyledDiv = styled.div`
-    //   display:flex;
-    //   margin: 50px auto;
-    //   padding: 30px;
-    //   display: flex;
-    //   flex-direction: column;
-    //   align-items: center;
-    // }
+    //styling
+    const StyledDiv = styled.div`
+      display:flex;
+      margin: 50px auto;
+      padding: 30px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+        
+   
+     
+      border:  2px solid;
+      color: black;
+      background-color: #FFF;
     
-    //   margin: 20px auto;
-    //   width: 63%;
-    //   border:  2px solid;
-    //   color: palevioletred;
-    //   background-color: white;
+      height: auto;
+      border-radius: 8px;
+      box-shadow: 0 1px 6px -2px #000;
+      overflow: hidden;
     
-    //   height: auto;
-    //   border-radius: 8px;
-    //   box-shadow: 0 19px 38px rgba(0, 0, 0, 0.8), 0 15px 12px rgba(0, 0, 0, 0.22);
-    //   overflow: hidden;
-    
-    //   &:hover {
-    //     background-color: palevioletred;
-    //     color: white;
-    //   }
-    // `;
+      &:hover {
+        background-color: red;
+        color: white;
+      }
+    `;
