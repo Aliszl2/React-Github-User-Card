226fff9eb50fd5e6f1500fc81862e2eaa455fdc9
Alison Law <alisparklaw@gmail.com>
2020-02-12 15:23:31 +0000

    Final tidy and reconfig

--- github-user-app/src/App.js
+++ github-user-app/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import GithubUserData from "./components/GithubUserData";
-import AliszlUserData from "./components/AliszlUserData";
 import img1 from "./assets/githublogo.png"
 import img2 from "./assets/lambdalogo.png"
 import "./App.css";
@@ -16,9 +15,6 @@ function App() {
         <img src={img1} alt="GitHub Logo" />
       </div>
       <div className="cards">
-        <h1>My GitHubData:</h1>
-      <AliszlUserData/>
-      <h1>My Folowers:</h1>
         <GithubUserData />
         </div>
     </div>
--- github-user-app/src/components/GithubUserData.jsx
+++ github-user-app/src/components/GithubUserData.jsx
@@ -1,4 +1,3 @@
-// import React, { useState, useEffect } from "react";
 import React from "react";
 import axios from "axios";
 import Card from "./Card";
@@ -7,24 +6,10 @@ export default class GithubUserData extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user1: {},
-      users: []
+       users: []
     };
   }
 
-  // componentDidMount() {
-  //   axios.get("https://api.github.com/users/Aliszl").then(response => {
-  //     console.log(response);
-  //     console.log(response.data);
-  //     console.log(this.state.users);
-  //     this.setState(oldState=>{
-  //       return{
-  //         user1: response.data
-  //       }
-      
-  //   });
-  //   });
-  // }
   componentDidMount() {
     axios
       .get("https://api.github.com/users/Aliszl/followers")
@@ -32,7 +17,7 @@ export default class GithubUserData extends React.Component {
        
         this.setState(oldState => {
           return {
-            users: response.data
+            users: [...response.data]
           };
         });
       })
@@ -40,14 +25,14 @@ export default class GithubUserData extends React.Component {
             axios
             .get("https://api.github.com/users/Aliszl")
             .then(response => {
-                  // console.log(response.data);
                   this.setState(oldState=>{
                 return{
-                  user1: response.data,
-                  users:[...this.state.users.concat(this.state.user1)]
-                  // users: oldState.users.unshift(user1)
-                  // users: oldState.users.concat(this.state.user1)
-                }
+                 
+                  users:[
+                    response.data,
+                    ...oldState.users
+                   ]
+                                }
               
             });
 
@@ -56,8 +41,7 @@ export default class GithubUserData extends React.Component {
   }
 
   render() {
-    console.log(this.state.user1);
-    console.log(this.state.users);
+
     return (
       <div>
         {
@@ -69,28 +53,4 @@ export default class GithubUserData extends React.Component {
   }
 }
 
-// export default function GithubUserData() {
-//   const [users, fetchUsers] = useState([]);
 
-//   useEffect(() => {
-
-//     axios
-//       .get("https://api.github.com/users/Aliszl/followers")
-
-//       .then(response => {
-
-//         fetchUsers(response.data);
-//       })
-//       .catch(error => {
-//         console.log("the data was not returned", error);
-//       });
-//   }, []);
-
-//   return (
-//     <div>
-//       {users.map(user => {
-//         return <Card user={user} />;
-//       })}
-//          </div>
-//   );
-// }
